99a6016d017e28e88d5acf931e269c878c8341fc
TomokiEmmei <0699559246@ae.k.u-tokyo.ac.jp>
2018-01-19 19:07:02 +0900

    Add a function to read from commnd line

--- multiplication.py
+++ multiplication.py
@@ -1,7 +1,9 @@
-x=9
-y=9
+# load numbers from command line
+x=int(input('X: '))
+y=int(input('Y: '))
 
-for i in range(1,x):
-	for j in range(1,y):
+# Display the multiplication table
+for i in range(1,x+1):
+	for j in range(1,y+1):
 		print(i*j, end=' ')
 	print()
