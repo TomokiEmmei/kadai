087852a150cc58015b3a6f618d340ad2072a50d9
TomokiEmmei <0699559246@ae.k.u-tokyo.ac.jp>
2018-01-19 20:21:20 +0900

    Add the addition table and command line argument

--- multiplication.py
+++ multiplication.py
@@ -1,9 +1,23 @@
+#command line argument
+# 'a' = addition 'm' = multipulication
+import sys
+args = sys.argv[1]
+
+
 # load numbers from command line
 x=int(input('X: '))
 y=int(input('Y: '))
 
 # Display the multiplication table
-for i in range(1,x+1):
-	for j in range(1,y+1):
-		print(i*j, end=' ')
-	print()
+if args == 'm':
+	for i in range(1,x+1):
+		for j in range(1,y+1):
+			print(i*j, end=' ')
+		print()
+
+# Display the addition table
+else:
+	for i in range(1,x+1):
+		for j in range(1,y+1):
+			print(i+j, end=' ')
+		print()
