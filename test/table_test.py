99e78ae525a1ef2a946b7fef12988eea257704fb
TomokiEmmei <0699559246@ae.k.u-tokyo.ac.jp>
2018-01-22 19:00:20 +0900

    add: add the unit test files

---
+++ test/table_test.py
@@ -0,0 +1,23 @@
+import unittest
+import multiplication
+
+class TestTable(unittest.TestCase):
+
+    def test_kakezan(self):
+        value1 = 3
+        value2 = 5
+        expected = [[1, 2, 3], [2, 4, 6,], [3, 6, 9], [4, 8, 12], [5, 10, 15]] 
+        actual = multiplication.kakezan(value1, value2)
+        self.assertEqual(expected, actual)
+        
+    def test_tashizan(self):
+        value1 = 3
+        value2 = 5
+        expected = [[2, 3, 4],[3, 4, 5], [4, 5, 6], [5, 6, 7], [6, 7, 8]]
+        actual = multiplication.tashizan(value1, value2)
+        self.assertEqual(expected, actual)
+            
+
+if __name__ == "__main__":
+    unittest.main()
+    
