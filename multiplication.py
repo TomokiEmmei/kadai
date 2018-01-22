62f658f526613c9da7b055dc030b93828884b33a
TomokiEmmei <0699559246@ae.k.u-tokyo.ac.jp>
2018-01-22 17:01:47 +0900

    update: line and row of table is converted
    add: fuction of tashizan and kakezan is added

--- multiplication.py
+++ multiplication.py
@@ -1,23 +1,32 @@
-#command line argument
-# 'a' = addition 'm' = multipulication
-import sys
-args = sys.argv[1]
+import sys #read command line argument
+import unittest
 
-
-# load numbers from command line
-x=int(input('X: '))
-y=int(input('Y: '))
-
+def kakezan(a,b):
 # Display the multiplication table
-if args == 'm':
-	for i in range(1,x+1):
-		for j in range(1,y+1):
+	for i in range(1,b+1):
+		for j in range(1,a+1):
 			print(i*j, end=' ')
 		print()
 
+def tashizan(a,b):
 # Display the addition table
-else:
-	for i in range(1,x+1):
-		for j in range(1,y+1):
+	for i in range(1,b+1):
+		for j in range(1,a+1):
 			print(i+j, end=' ')
 		print()
+
+def main():
+#command line argument
+# 'a' = addition 'm' = multipulication
+    args = sys.argv[1]
+    #load numbers from command line
+    x=int(input('x: '))
+    y=int(input('y: '))
+    print(2)
+    if args == 'm':
+        kakezan(x,y)
+    else:
+        tashizan(x,y)
+        
+if __name__ == '__main__':
+    main()
