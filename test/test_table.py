f07a1d4cce8da47368480221178caef654bacbd9
TomokiEmmei <0699559246@ae.k.u-tokyo.ac.jp>
2018-01-23 19:20:49 +0900

    update: add some comments to the test_table.py

--- test/test_table.py
+++ test/test_table.py
@@ -7,15 +7,15 @@ class TestTable(unittest.TestCase):
         value1 = 3
         value2 = 5
         expected = [[1, 2, 3], [2, 4, 6,], [3, 6, 9], [4, 8, 12], [5, 10, 15]] 
-        actual = multiplication.kakezan(value1, value2)
-        self.assertEqual(expected, actual)
+        actual = multiplication.kakezan(value1, value2) #run kakezan()
+        self.assertEqual(expected, actual) #compare expected with actual result
         
     def test_tashizan(self):
         value1 = 3
         value2 = 5
         expected = [[2, 3, 4],[3, 4, 5], [4, 5, 6], [5, 6, 7], [6, 7, 8]]
-        actual = multiplication.tashizan(value1, value2)
-        self.assertEqual(expected, actual)
+        actual = multiplication.tashizan(value1, value2) #run kakezan()
+        self.assertEqual(expected, actual) #compare expected with actual result
             
 
 if __name__ == "__main__":
